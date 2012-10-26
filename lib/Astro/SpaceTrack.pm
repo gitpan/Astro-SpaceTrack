57143b8816d4ebb37dede77f23b680c066c71ea7
Tom Wyant <wyant@cpan.org>
2012-10-26 23:45:47 +0000

    Import of WYANT/Astro-SpaceTrack-0.068 from CPAN.
    gitpan-cpan-distribution: Astro-SpaceTrack
    gitpan-cpan-version:      0.068
    gitpan-cpan-path:         WYANT/Astro-SpaceTrack-0.068.tar.gz
    gitpan-cpan-author:       WYANT
    gitpan-cpan-maturity:     released

--- lib/Astro/SpaceTrack.pm
+++ lib/Astro/SpaceTrack.pm
@@ -206,7 +206,7 @@ use warnings;
 
 use base qw{Exporter};
 
-our $VERSION = '0.067';
+our $VERSION = '0.068';
 our @EXPORT_OK = qw{shell BODY_STATUS_IS_OPERATIONAL BODY_STATUS_IS_SPARE
     BODY_STATUS_IS_TUMBLING};
 our %EXPORT_TAGS = (
@@ -2347,7 +2347,7 @@ sub _retrieve_v2 {
 	my @batch = splice @args, 0, $RETRIEVAL_SIZE;
 	$rest->{NORAD_CAT_ID} = _stringify_oid_list( {
 		separator	=> ',',
-		range_operator	=> '--',
+		range_operator	=> _rest_range_operator(),
 	    }, @batch );
 
 	my $resp = $self->spacetrack_query_v2(
@@ -2541,7 +2541,7 @@ sub _retrieve_v2 {
 	    @args = (
 		_stringify_oid_list( {
 			separator	=> ',',
-##			range_operator	=> '--',
+			range_operator	=> _rest_range_operator(),
 		    },
 		    @args
 		)
@@ -5785,6 +5785,15 @@ sub _search_generic_tabulate {
 
 	return @rslt;
     }
+}
+
+# The following UNDOCUMENTED hack will disappear when the REST
+# interface's behavior when you have ranges in a list of OIDs
+# stabilizes.
+sub _rest_range_operator {
+    return $ENV{SPACETRACK_REST_RANGE_OPERATOR} ?
+	'--' :
+	undef;
 }
 
 #	$swapped = _swap_upper_and_lower( $original );
