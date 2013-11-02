bb05f70fa45c41a9feb1857de7e190a7998cae3d
Tom Wyant <wyant@cpan.org>
2013-11-02 19:54:44 +0000

    Import of WYANT/Astro-SpaceTrack-0.081_02 from CPAN.
    gitpan-cpan-distribution: Astro-SpaceTrack
    gitpan-cpan-version:      0.081_02
    gitpan-cpan-path:         WYANT/Astro-SpaceTrack-0.081_02.tar.gz
    gitpan-cpan-author:       WYANT
    gitpan-cpan-maturity:     developer

--- lib/Astro/SpaceTrack.pm
+++ lib/Astro/SpaceTrack.pm
@@ -119,7 +119,7 @@ use warnings;
 
 use base qw{Exporter};
 
-our $VERSION = '0.081_01';
+our $VERSION = '0.081_02';
 our @EXPORT_OK = qw{shell BODY_STATUS_IS_OPERATIONAL BODY_STATUS_IS_SPARE
     BODY_STATUS_IS_TUMBLING};
 our %EXPORT_TAGS = (
@@ -128,7 +128,7 @@ our %EXPORT_TAGS = (
 );
 
 use Carp;
-use Getopt::Long 2.33;
+use Getopt::Long 2.39;
 use HTTP::Response;
 use HTTP::Status qw{
     HTTP_PAYMENT_REQUIRED
